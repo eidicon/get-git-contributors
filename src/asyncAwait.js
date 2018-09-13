41c1a9e7304e5f6ac0737a333907f12027fe31c3
Oleksii Bochurkin <oleksii.bochurkin@echovox.com>
2018-09-13 17:49:26 +0300

    updated async/await

--- src/asyncAwait.js
+++ src/asyncAwait.js
@@ -6,18 +6,20 @@ const getContributorsA = async (orgainzation) => {
     let headers = await repos.headers
     let header = headers[Object.getOwnPropertySymbols(headers)[0]]
     let date = await new Date(header['x-ratelimit-reset'] * 1000)
-    return `Limit: ${header['x-ratelimit-remaining']} left of ${header['x-ratelimit-limit']}. Wait until ${date.toTimeString()}`
+    throw new Error(`Limit: ${header['x-ratelimit-remaining']} left of ${header['x-ratelimit-limit']}. Wait until ${date.toTimeString()}`)
   }
   let reposJson = await repos.json()
-  return contributorsPerRepo(reposJson, orgainzation)
+  let data = await contributorsPerRepo(reposJson, orgainzation)
+  return data.reduce((acc, curr) => {
+    return Object.assign(acc, curr)
+  }, {})
 }
 
 const contributorsPerRepo = async (repoList, orgainzation) => {
-  return repoList.filter((item, key) => { if (key < 3) { return item } }).reduce(async (collection, current) => {
-    let acc = await collection
-    await Object.assign(acc, { [current.name]: await fetchContributors(current.name, orgainzation) })
-    return acc
-  }, Promise.resolve({}))
+  let repos = await repoList.slice(0, 3)
+  let [firstP, secondP, thirdP] = await repos.map(async (item) => ({ [item.name]: await fetchContributors(item.name, orgainzation) }))
+  let object = Promise.all([firstP, secondP, thirdP])
+  return object
 }
 
 const fetchContributors = async (name, orgainzation) => {
--- src/promise.js
+++ src/promise.js
@@ -13,25 +13,21 @@ const getContributorsB = (orgainzation) => {
         }
       })
       .then(resp => resp.json())
-      .then(json => selectRepos(json, orgainzation).then(resp => resp))
+      .then(json => selectRepos(json, orgainzation).resolve())
+      .then(promise => console.log(promise))
       .catch(err => ({ 'err': err }))
   })
 }
 
 const selectRepos = (data, orgainzation) => {
   return new Promise((resolve, reject) => {
-    resolve(data
+    data
       .filter((item, key) => { if (key < 3) { return item } })
       .reduce((promise, current) => {
-        return promise
-          .then(result => {
-            return fetchContributors(current.name, orgainzation)
-              .then(arr => Object.assign(promise, { [current.name]: arr }))
-              .catch(err => console.log(err))
-          })
-          .catch(err => console.log(err))
-      }, Promise.resolve())
-    )
+        return promise.then(result => {
+          fetchContributors(current.name, orgainzation).then(arr => Object.assign(promise, { [current.name]: arr }))
+        })
+      }, Promise.resolve({}))
   })
 }
 
@@ -39,7 +35,10 @@ const fetchContributors = (name, orgainzation) => {
   return new Promise((resolve, reject) => {
     fetch(`https://api.github.com/repos/${orgainzation}/${name}/contributors`)
       .then(resp => resp.json())
-      .then(json => resolve(json.map(contrib => contrib.login)))
+      .then(json => {
+        console.log(json)
+        return resolve(json.map(contrib => contrib.login))
+      })
       .catch(err => reject(err))
   })
 }
